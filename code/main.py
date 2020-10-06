1be17de4d40dda43aa08707f710626ed278b467d
Francky_ <frederick_spore@hotmail.com>
2020-10-06 20:45:29 +0200

    opkuisen code

--- code/main.py
+++ code/main.py
@@ -6,7 +6,7 @@ from dth import DTH
 from hx711 import HX711
 
 
-#Pins op het motor control board
+#Pins op van de pycom
 enableA = "P12"
 enableB = "P7"
 in1 = "P11"
@@ -18,10 +18,6 @@ data_pin = "P6"
 clk_pin = "P5"
 switch_pin = "P13"
 
-#snelheid variable van 1 tot 100
-speed = 100
-
-
 
 #initialiseer alle pins van de pycom
 enable_gate =   Pin(enableA, mode=Pin.OUT)
@@ -32,9 +28,8 @@ enable_fan =   Pin(enableB, mode=Pin.OUT)
 fan_1 = Pin(in3, mode=Pin.OUT)
 fan_2 = Pin(in4, mode=Pin.OUT)
 
-
+#schakelaar voor de poort open te doen
 switch = Pin(switch_pin, mode = Pin.IN)
-
 
 #maak een pwm aan met timer 0 & frequentie van 5KHz
 pwm = PWM(0, frequency=5000)
@@ -42,33 +37,32 @@ pwm = PWM(0, frequency=5000)
 #creer een pwm kaneel op pin enableA met een duty cycle van 0
 gate_speed = pwm.channel(0, pin=enableA, duty_cycle=0)
 fan_speed = pwm.channel(0, pin=enableB, duty_cycle=0)
+#deze worden niet gebruikt want de motoren bewegen pas vanaf 90% duty cycle
 
-
-
+#de load cell amplifier
 load_amp = HX711(data_pin, clk_pin)
-#load_amp.tare()
+
+#wordt gebruikt om de load cell waarde op null te zetten
 weight_offset = 969850
 
 def main():
-    isOpen = False
+    #kijkt of de poort open of dicht is en sluit/opent deze als de schakelaar gebruikt wordt (zie video)
+    gateIsOpen = False
     while True:
-        #print("started")
-        if(switch.value() and isOpen):
+        if(switch.value() and gateIsOpen):
             print("gate is closing")
             close_gate()
             isOpen = False
-        elif(switch.value() and (not isOpen)):
+        elif(switch.value() and (not gateIsOpen)):
             print("gate is opening")
             open_gate()
             isOpen = True
 
-        print(load_amp.get_value() + weight_offset )
+        #print(load_amp.get_value() + weight_offset )
+        #read_dht()
 
-    #read_dht()
-    #gewicht.append(load_amp.get_value())
-
-    #print(switch.value())
-
+#WIP
+#functie om het gewicht te lezen van de load cell
 def weight():
     while True:
         time.sleep(5)
@@ -81,14 +75,7 @@ def weight():
         diff = first - second
         print("gewicht   {}".format(diff/1500))
 
-
-def read_load_cell():
-    value = hx711.read()
-    value = hx711.get_value()
-    print(value)
-    print("ok")
-
-
+#leest de waardes van dht en print deze
 def read_dht():
     th = DTH(dht_pin,1)
     result = th.read()
@@ -98,28 +85,31 @@ def read_dht():
     else:
         print("Invalid Result")
 
+#start de ventilator
 def start_fan():
     fan_2.value(0)
     fan_1.value(1)
     enable_fan.value(1)
 
+#stopt de ventilator
 def stop_fan():
     enable_fan.value(0)
     fan_2.value(0)
     fan_1.value(0)
 
-
+#opent de poort
 def open_gate():
     gate_open.value(1)
-    time.sleep(44) #hoelang het duurt om de poort open te doen
+    time.sleep(44) #hoelang het duurt om de poort open te doen in seconden
     stop_gate()
 
+#sluit de poort
 def close_gate():
     gate_close.value(1)
-    time.sleep(41) #hoelang het duurt om de poort dicht te doen
+    time.sleep(41) #hoelang het duurt om de poort dicht te doen in seconden
     stop_gate()
 
-
+#stopt de motor die de poort aanstuurt
 def stop_gate():
     enable_gate.value(0)
     gate_close.value(0)
